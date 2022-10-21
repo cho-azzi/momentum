a80e44dd298a3e526cd1d895377076a33ab20bbe
cho-azzi <azziewha@ewhain.net>
2022-10-21 13:09:40 +0900

    background img changed

--- js/background.js
+++ js/background.js
@@ -5,7 +5,11 @@ const images=[
      "3.jpg",
      "4.jpg",
      "5.jpg",
-     "6.jpg"
+     "6.jpg",
+     "7.jpg",
+     "8.jpg",
+     "9.jpg",
+     "10.jpg"
 ];
 
 const chosenImage=images[Math.floor(Math.random()*images.length)];
