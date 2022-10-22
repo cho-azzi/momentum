b16dae7b230fc448f6199645927ffd74b06ee920
cho-azzi <azziewha@ewhain.net>
2022-10-22 20:08:36 +0900

    timer bug
    start , reset 버튼 여러번 눌렀을 때 생기는 문제

--- js/timer.js
+++ js/timer.js
@@ -2,7 +2,10 @@ const timerForm=document.getElementById("timer-form");
 const start=document.getElementById("start");
 const reset=document.getElementById("reset");
 const neoguri=document.getElementById("neoguri");
-const line=document.querySelector("#timer-line div div")
+const line=document.querySelector("#timer-line div div");
+
+let oneByOne =0;
+let oneByOneRESET=0;
 
 let movingPixel;
 let movingIndex=1;
@@ -18,7 +21,8 @@ function timer(){
    let m =parseInt(minutes.value);
    let s =parseInt(seconds.value);
   
-
+if(m!=NaN&&s!=NaN)
+{
   if(m==0&&s==0){
      minutes.value="0";
      seconds.value="0";
@@ -30,7 +34,6 @@ function timer(){
   }else if(s!=0){
      s--;
      seconds.value=String(s);
-     console.log(`${movingPixel*movingIndex}`);
      neoguri.style.transform = `translateX(${movingPixel*movingIndex}px)`;
      line.style.width=`${430-movingPixel*movingIndex}px`;
 
@@ -38,7 +41,6 @@ function timer(){
   }else if(m!=0&&s==0){
      s=59;
      m--;
-     console.log(`${movingPixel*movingIndex}`);
      seconds.value=String(s);
      minutes.value=String(m);
      neoguri.style.transform = `translateX(${movingPixel*movingIndex}px)`;
@@ -46,7 +48,9 @@ function timer(){
 
 
   }
-
+}else{
+     stopTimer();
+}
   
 
 }
@@ -55,7 +59,9 @@ function stopTimer(){
      clearInterval(startTimer);
      neoguri.style.transform = "translateX(0px)";
      line.style.width="430px";
-
+     minutes.value="0";
+     seconds.value="0";
+     oneByOne=0;
 
 
 }
@@ -64,6 +70,12 @@ function stopTimer(){
 
 function onhandleTimer(event){
 event.preventDefault();
+oneByOne++;
+console.log(oneByOne)
+if(oneByOne>=2) {alert("start 버튼은 한 번만 누르세요!");
+oneByOne=1;
+return;}
+
 if(parseInt(minutes.value)>=0&&parseInt(seconds.value)>=0){
      startInterval();
      movingPixel= 480/( parseInt(minutes.value)*60+parseInt(seconds.value));
@@ -72,6 +84,7 @@ if(parseInt(minutes.value)>=0&&parseInt(seconds.value)>=0){
      alert("0이상의 정수를 입력해주세요!");
      neoguri.style.transform = "translateX(0px)";
      line.style.width="430px";
+     stopTimer();
 
      return;
 }
@@ -89,7 +102,6 @@ function startInterval(){
 start.addEventListener("click",onhandleTimer);
 
 reset.addEventListener("click", function(){
-     minutes.value="0";
-     seconds.value="0";
+
 stopTimer();
 })
